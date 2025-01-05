21cf04a1280e47fe0b5e99590f222732258ff8b7
Bram van Oosterhout <bram@van-oosterhout.org>
2025-01-06 10:51:47 +1100

    Ntart node. Fail to connect to non-existent identity server

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -3,7 +3,7 @@
 # define host names for the servers
 
 # start servers
-#systemctl start mysql
-#systemctl start festivals-database-node
+systemctl start mysql
+systemctl start festivals-database-node
 # Keeps the container alive 
 while true; do sleep 1s; done
