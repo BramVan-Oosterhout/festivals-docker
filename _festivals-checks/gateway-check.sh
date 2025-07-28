95a189bf48f35d3b94fc288484d1108fd03bfa5a
Bram van Oosterhout <bram@van-oosterhout.org>
2025-07-28 12:40:47 +1000

    add database-check. Fix spekking in gateway check

--- _festivals-checks/gateway-check.sh
+++ _festivals-checks/gateway-check.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-JWT=$(curl -v -v -H "Api-Key: TEST_API_KEY_001" \
+JWT=$(curl -H "Api-Key: TEST_API_KEY_001" \
          -u "admin@email.com:we4711" \
          --cert /usr/local/festivals-checks/api-client.crt \
          --key /usr/local/festivals-checks/api-client.key \
          --cacert /usr/local/festivals-checks/ca.crt \
          https://festivals-identity-server:22580/users/login)
 
-echo '==========================================================================='
+echo '=============================== INFO ============================================'
 
-curl -v -v -H "Api-Key: TEST_API_KEY_001" \
+curl -H "Api-Key: TEST_API_KEY_001" \
      -H "Authorization: Bearer $JWT" \
      --cert /usr/local/festivals-checks/api-client.crt \
      --key /usr/local/festivals-checks/api-client.key \
      --cacert /usr/local/festivals-checks/ca.crt \
-     https://gateway.festivals-gateway/info
+     https://gateway.festivals-gateway/info | jq
 
 echo
-echo '==========================================================================='
-#curl -H "Api-Key: TEST_API_KEY_001" \
-#     -H "Authorization: Bearer $JWT" \
-#     --cert /opt/homebrew/etc/pki/issued/api-client.crt \
-#     --key /opt/homebrew/etc/pki/private/api-client.key \
-#     --cacert /opt/homebrew/etc/pki/ca.crt \
-#     https://gateway.festivalsapp.home/info
+echo '============================== SERVICES ============================================='
+curl -H "Api-Key: TEST_API_KEY_001" \
+     -H "Authorization: Bearer $JWT" \
+     --cert /usr/local/festivals-checks/api-client.crt \
+     --key /usr/local/festivals-checks/api-client.key \
+     --cacert /usr/local/festivals-checks/ca.crt \
+     https://discovery.festivals-gateway/services | jq
