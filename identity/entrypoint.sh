004d50b928f23f91d5bc5899c763528347fee8ee
Bram van Oosterhout <bram@van-oosterhout.org>
2025-08-06 15:08:00 +1000

    cleanup gateway, identity and database dirs

--- identity/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# define host names for the servers
-
-# start servers
-systemctl start mysql
-systemctl start festivals-identity-server
-# Keeps the container alive 
-while true; do sleep 1s; done
