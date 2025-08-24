e212b1d1dd83017b4c85fac1340eb03fea6f5bba
Bram van Oosterhout <bram@van-oosterhout.org>
2025-08-24 10:25:42 +1000

    add [erl module for server checks

--- _festivals-checks/postJSONtoURL.pl
+++ _festivals-checks/postJSONtoURL.pl
@@ -6,6 +6,7 @@ use strict;
 use strict;
 use warnings;
 use LWP::UserAgent;
+use HTTP::Request;
 use HTTP::Request::Common; # Use HTTP::Request directly for more control
 use JSON; # For encoding Perl data structures to JSON
 
@@ -15,9 +16,15 @@ my $url = 'https://festivals-fileserver:1910/images/upload';
 my $file = 'postJSONtoURL.pl';
 my $JWT = get_jwt_from_festivals();
 
-my $response = post_file_to_url( $file, $url, $JWT );
+my $response = modify_hash_for_url(
+    'PATCH',
+    { 'festival_name' => 'nff2025' },
+    'https://festivals-server:10439/festivals/3',
+    $JWT);
 
-print $response, "\n";
+#my $response = post_file_to_url( $file, $url, $JWT );
+
+#print $response, "\n";
 
 sub get_jwt_from_festivals {
     my $url = 'https://festivals-identity-server:22580/users/login';
@@ -35,7 +42,6 @@ sub get_jwt_from_festivals {
 
     return $response->content;
 }
-
 
 sub post_file_to_url {
     my ( $file, $url, $JWT ) = @_;
@@ -56,26 +62,28 @@ sub post_file_to_url {
 }
 
 
-sub post_json_to_url {
+sub modify_hash_for_url {
+    my ( $method, $hash, $url, $JWT ) = @_;
+    
+    my $jsonContent = encode_json($hash); # Encode the Perl hash to JSON
 
-    my $url = 'https://festivals-fileserver:1910/files';
+    my $request = HTTP::Request->new( 
+                  $method, $url,
+                  ['Content-Type', 'application/json',
+                  'Api-Key', 'TEST_API_KEY_001',
+                  'Authorization', "Bearer $JWT"],
+                  $jsonContent);
+
     my $ua = LWP::UserAgent->new;
     $ua->ssl_opts ( SSL_ca_file =>  '/usr/local/festivals-checks/ca.crt',
                     SSL_cert_file => '/usr/local/festivals-checks/api-client.crt',
                     SSL_key_file => '/usr/local/festivals-checks/api-client.key');
 
-    my $data = {
-        image => 'fileserver-check.sh',
-    };
-
-    my $json_content = encode_json($data); # Encode the Perl hash to JSON
-
-    my $request = HTTP::Request->new(POST => $url);
-    $request->header('Content-Type' => 'application/json',
-                    'Api-Key' => 'TEST_API_KEY_001');
-    $request->content($json_content);
-
+   
     my $response = $ua->request($request);
+
+    print dump($response), "\n";
+    exit;
 
     if ($response->is_success) {
         print "JSON POST successful!\n";
@@ -85,40 +93,3 @@ sub post_json_to_url {
     }
 
 }__END__
-Exampleof file upload
-Search : lwp useragent upload file
----
-#!/usr/bin/perl
-use strict;
-use warnings;
-use LWP::UserAgent;
-use HTTP::Request::Common; # Provides POST, PUT, etc.
-
-my $ua = LWP::UserAgent->new;
-my $upload_url = 'http://example.com/upload'; # Replace with your upload URL
-my $local_file_path = 'my_file.txt'; # Replace with the path to your local file
-
-sub post_file_to_url {
-    my ( $file, $url, $jwt ) = @_;
-}
-
-# Ensure the file exists before attempting to upload
-die "File not found: $local_file_path" unless -e $local_file_path;
-
-my $response = $ua->post(
-    $upload_url,
-    Content_Type => 'form-data',
-    Content => [
-        file_field_name => [$local_file_path], # 'file_field_name' is the name of the file input field on the server
-        # You can add other form fields here if needed:
-        # other_field => 'some_value',
-    ],
-);
-
-if ($response->is_success) {
-    print "File uploaded successfully!\n";
-    print $response->decoded_content;
-} else {
-    print "File upload failed: " . $response->status_line . "\n";
-    print $response->decoded_content if $response->decoded_content;
-}
