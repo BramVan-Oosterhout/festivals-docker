d6e52dda0eeaeee8921aff1a82cc28c62cefe9b6
Bram van Oosterhout <bram@van-oosterhout.org>
2025-10-05 16:51:12 +1100

    Add FestivalsPlugin and nginx.conf

---
+++ festivals-ui/FestivalsPlugin_installer.pl
@@ -0,0 +1,171 @@
+#! /usr/bin/env perl
+#
+# Install script for FestivalsPlugin
+#
+# Copyright (C) 2004-2019 Foswiki Contributors. All Rights Reserved.
+# Foswiki Contributors are listed in the AUTHORS file in the root of
+# this distribution. NOTE: Please extend that file, not this notice.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# As per the GPL, removal of this notice is prohibited.
+#
+# Author: Crawford Currie http://c-dot.co.uk
+#
+# NOTE TO THE DEVELOPER: THIS FILE IS GENERATED AUTOMATICALLY
+# BY THE BUILD PROCESS DO NOT EDIT IT - IT WILL BE OVERWRITTEN
+#
+use strict;
+use warnings;
+require 5.008;
+
+use File::Spec;
+use Cwd;
+
+=pod
+
+---+ FestivalsPlugin_installer
+This is the installer script. The basic function of this script is to
+locate an archive and unpack it.
+
+It will also check the dependencies listed in DEPENDENCIES and assist
+the user in installing any that are missing. The script also automatically
+maintains the revision histories of any files that are being installed by the
+package but already have ,v files on disc (indicating that they are
+revision controlled).
+
+The script also functions as an *uninstaller* by passing the parameter
+=uninstall= on the command-line. Note that uninstallation does *not* revert
+the history of any topic changed during the installation.
+
+The script allows the definition of PREINSTALL and POSTINSTALL scripts.
+These scripts can be used for example to modify the configuration during
+installation, using the functions described below.
+
+Refer to the documentation of =configure=
+
+=cut
+
+# This is all done in package Foswiki so that reading LocalSite.cfg and Foswiki.cfg
+# will put the config vars into the right namespace.
+package Foswiki;
+
+# The root of package URLs
+my $PACKAGES_URL = 'https://foswiki.org/pub/Extensions';
+
+# Extract MANIFEST and DEPENDENCIES from the __DATA__
+undef $/;
+my @DATA = split( /<<<< (.*?) >>>>\s*\n/, <DATA> );
+shift @DATA;    # remove empty first element
+
+# Establish where we are
+my @path = ( 'tools', 'extender.pl' );
+my $wd = Cwd::cwd();
+$wd =~ /^(.*)$/;    # untaint
+unshift( @path, $1 ) if $1;
+my $script = File::Spec->catfile(@path);
+
+unless ( my $return = do $script ) {
+    my $message = <<MESSAGE;
+************************************************************
+Could not load $script
+
+Change to the root directory of your Foswiki installation
+before running this installer.
+
+MESSAGE
+    if ($@) {
+        $message .= "There was a compile error: $@\n";
+    }
+    elsif ( defined $return ) {
+        $message .= "There was a file error: $!\n";
+    }
+    else {
+        $message .= "An unspecified error occurred\n";
+    }
+
+    # Try again, using open. This cures some uncooperative platforms.
+    if ( open( F, '<', $script ) ) {
+        local $/;
+        my $data = <F>;
+        close(F);
+        $data =~ /^(.*)$/s;    # untaint
+        eval $1;
+        if ($@) {
+            $message .= "Error when trying to eval the file content: $@\n";
+        }
+        else {
+            print STDERR
+              "'do $script failed, but install was able to proceed: $message";
+            undef $message;
+        }
+    }
+    else {
+        $message .= "Could not open file using open() either: $!\n";
+    }
+    die $message if $message;
+}
+
+sub preuninstall {
+
+    # # No PREUNINSTALL script;
+}
+
+sub postuninstall {
+
+    # # No POSTUNINSTALL script;
+}
+
+sub preinstall {
+
+    # # No PREINSTALL script;
+}
+
+sub postinstall {
+
+    # # No POSTINSTALL script;
+}
+
+Foswiki::Extender::install( $PACKAGES_URL, 'FestivalsPlugin', 'FestivalsPlugin',
+    @DATA );
+
+1;
+
+# MANIFEST and DEPENDENCIES are done this way
+# to make it easy to extract them from this script.
+
+__DATA__
+<<<< MANIFEST >>>>
+data/System/FestivalsPlugin.txt,0644,1e00629874780a162a4ad8b168803994,
+data/Sandbox/FestivalsApp.txt,0644,10cc0a7970f8c9ea151cb36c30e16714,
+data/Sandbox/FestivalsEventsByDay.txt,0644,fb85a5a58a429d9f8708d7783a2df322,
+data/Sandbox/FestivalsArtist.txt,0644,cf0ce4d8424c698bebe015f6f94994ea,
+data/Sandbox/FestivalsArtistsAtoZ.txt,0644,f92c729f14973b22dce7a621d8c443b4,
+lib/Foswiki/Plugins/FestivalsPlugin.pm,0644,e619d510bd981e11ad517a2af9a72375,
+lib/Foswiki/Plugins/FestivalsPlugin/Config.spec,0644,4b5bfcdb01d2f6abc74406dafb763575,
+lib/Foswiki/Plugins/FestivalsPlugin/Core.pm,0664,9bd0e2b372213ad35b1c7dcaab1ccf1d,
+lib/Foswiki/Plugins/FestivalsPlugin/DEPENDENCIES,0664,b7697f5caa1e10476567636b377bae00,
+lib/Foswiki/Plugins/FestivalsPlugin/FestivalsAgent.pm,0664,e2d5501cda810851e09f292c487585b0,
+
+<<<< DEPENDENCIES >>>>
+Data::Dump,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin.pm, lib/Foswiki/Plugins/FestivalsPlugin.pm, lib/Foswiki/Plugins/FestivalsPlugin/Core.pm, lib/Foswiki/Plugins/FestivalsPlugin/FestivalsAgent.pm, lib/Foswiki/Plugins/FestivalsPlugin/test_get_all.pl
+Error,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin/Core.pm
+Foswiki::Func,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin.pm, lib/Foswiki/Plugins/FestivalsPlugin.pm, lib/Foswiki/Plugins/FestivalsPlugin/Core.pm
+Foswiki::Plugins,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin.pm, lib/Foswiki/Plugins/FestivalsPlugin.pm
+HTTP::Request,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin/FestivalsAgent.pm
+HTTP::Request::Common,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin/FestivalsAgent.pm
+JSON,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin/FestivalsAgent.pm
+LWP::UserAgent,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin/FestivalsAgent.pm
+Time::Piece,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin/Core.pm
+Try::Tiny,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin/FestivalsAgent.pm
+Unit::TestSuite,>=0,1,cpan,May be required for test/unit/FestivalsPlugin/FestivalsPluginSuite.pm
+lib,>=0,1,cpan,May be required for lib/Foswiki/Plugins/FestivalsPlugin/test_get_all.pl
+
